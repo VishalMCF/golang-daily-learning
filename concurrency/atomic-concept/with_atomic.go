d3b63dd8789e388a9226c920fc8e3df74f5b1bb0
Vishal M Yadav <vishal.yadav@embibe.com>
2024-04-16 15:51:46 +0530

    added logic for atomic

--- concurrency/atomic-concept/with_atomic.go
+++ concurrency/atomic-concept/with_atomic.go
@@ -30,3 +30,20 @@ func WithAtomicDemo() {
 	wg.Wait()
 	fmt.Println("ops:", ops.Load())
 }
+
+//func WithAtomicDemo() {
+//	var ops uint64
+//	var wg sync.WaitGroup
+//
+//	for i := 0; i < 100; i++ {
+//		wg.Add(1)
+//		go func() {
+//			for c := 0; c < 100; c++ {
+//				atomic.AddUint64(&ops, 1)
+//			}
+//			wg.Done()
+//		}()
+//	}
+//	wg.Wait()
+//	fmt.Println("ops:", atomic.LoadUint64(&ops))
+//}
