b214d4e5762f943c9e7cc55fbad92e6d4704839f
Vishal M Yadav <vishal.yadav@embibe.com>
2024-04-16 15:26:27 +0530

    added demo for atomic code demo

--- concurrency/atomic-concept/with_atomic.go
+++ concurrency/atomic-concept/with_atomic.go
@@ -1,1 +1,32 @@
 package atomic_concept
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+/**
+func AddInt64(address *int64, delta int64)(new int64)
+func LoadInt64(address *int64) (value int64)
+func StoreInt64(address *int64, value int64)
+func SwapInt64(address *int64, new int64) (old int64)
+func CompareAndSwapInt64(address *int64, old, new int64) (swapped bool)
+*/
+
+func WithAtomicDemo() {
+	var ops atomic.Uint64
+	var wg sync.WaitGroup
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func() {
+			for c := 0; c < 100; c++ {
+				ops.Add(1)
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println("ops:", ops.Load())
+}
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
+import atomic_concept "event-package-test/concurrency/atomic-concept"
+
 func main() {
 	// examples.OneToMany()
 	// examples.ExampleBaseServer()
 	// examples.ExampleEventAndChallels()
 	// mutexes_concept.Demo()
 	// atomic_concept.NonAtomicDemo()
+	atomic_concept.WithAtomicDemo()
 }
